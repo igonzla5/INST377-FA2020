243b2797a239d8d90fe7e754650cb8289c4a988e
Israel Gonzalez <igonzal5@terpmail.umd.edu>
2020-10-08 22:25:28 -0400

    header

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
-    res.send(`Lab 5 for Israel Gonzalez`);
+    res.send(`Lab 5 for ${process.env.NAME}`);
   })
   .post((req, res) => {
     res.send("hello world");
